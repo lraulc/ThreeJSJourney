4127b14f09fadb39cf72cde2fcbd31b8e0ba1019
GRAYSCALEWICHO\lraul <lraul.castillo@gmail.com>
2023-07-31 17:51:33 -0600

    orbit controls and basic camera movement

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from "gsap";
 
 /*
@@ -11,8 +12,6 @@ const cursor = {
 window.addEventListener("mousemove", (event) => {
   cursor.mouseX = event.clientX / sizes.width - 0.5;
   cursor.mouseY = -(event.clientY / sizes.height - 0.5);
-  console.log(cursor.mouseX);
-  console.log(cursor.mouseY);
 });
 
 // Canvas
@@ -35,17 +34,22 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-camera.position.set(0, 0, 2);
+camera.position.set(3, 2, 3);
 camera.lookAt(0, 0, 0);
 scene.add(camera);
 
 // Object
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshNormalMaterial();
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.position.y = boxMesh.scale.y / 2;
 scene.add(boxMesh);
+
+// Camera Controls
+// Add camera, and DOM element - this case, canvas
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(boxMesh.position.x, boxMesh.position.y, boxMesh.position.z);
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -55,8 +59,8 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 /*******************************************
-                  Helpers 
-*******************************************/
+ Helpers 
+ *******************************************/
 
 const gridHelper = new THREE.GridHelper(10, 10, 0x00eeff, 0xffffff);
 const axesHelper = new THREE.AxesHelper(3);
@@ -71,18 +75,17 @@ const clock = new THREE.Clock();
 const tick = () => {
   const deltaTime = clock.getElapsedTime();
 
-  // boxMesh.rotation.y = 1 * deltaTime;
+  // // Update Camera
+  // const cameraSpeed = Math.PI;
+  // camera.position.set(
+  //   Math.sin(cursor.mouseX * Math.PI * 2) * cameraSpeed,
+  //   cursor.mouseY * Math.PI * 2,
+  //   Math.cos(cursor.mouseX * Math.PI * 2) * cameraSpeed
+  // );
+  // camera.lookAt(boxMesh.position);
 
-  // Update Camera
-  const cameraSpeed = Math.PI;
-  camera.position.set(
-    Math.sin(cursor.mouseX * Math.PI * 2) * cameraSpeed,
-    cursor.mouseY * Math.PI * 2,
-    Math.cos(cursor.mouseX * Math.PI * 2) * cameraSpeed
-  );
-
-  camera.lookAt(boxMesh.position);
-
+  // Controls have to be updated when using damping
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
