7200eca879937566b1ac608152770db282774411
GRAYSCALEWICHO\lraul <lraul.castillo@gmail.com>
2023-07-31 17:55:36 -0600

    starting fullscreen and resizing

--- src/script.js
+++ src/script.js
@@ -1,43 +1,93 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/*
+ *Cursor
+ */
+const cursor = {
+  mouseX: 0,
+  mouseY: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.mouseX = event.clientX / sizes.width - 0.5;
+  cursor.mouseY = -(event.clientY / sizes.height - 0.5);
+});
 
 // Canvas
-const canvas = document.querySelector('.webgl');
-
+const canvas = document.querySelector(".webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
 // Sizes
-const sizes = 
-{
-    width: 800,
-    height:800
+const sizes = {
+  width: 800,
+  height: 800,
 };
-
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.01,
-    1000
+  75,
+  sizes.width / sizes.height,
+  0.01,
+  1000
 );
-camera.position.z = 3
+
+camera.position.set(3, 2, 3);
+camera.lookAt(0, 0, 0);
 scene.add(camera);
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshNormalMaterial();
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.position.y = boxMesh.scale.y / 2;
+scene.add(boxMesh);
+
+// Camera Controls
+// Add camera, and DOM element - this case, canvas
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(boxMesh.position.x, boxMesh.position.y, boxMesh.position.z);
+controls.zoomSpeed = 0.1;
+controls.enableDamping = true;
 
 // Renderer
-const renderer = new THREE.WebGLRenderer(
-    {
-        canvas: canvas
-    }
-);
-renderer.setSize(sizes.width,sizes.height);
-renderer.render(scene,camera);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+/*******************************************
+ Helpers 
+ *******************************************/
+
+const gridHelper = new THREE.GridHelper(10, 10, 0x00eeff, 0xffffff);
+const axesHelper = new THREE.AxesHelper(3);
+const helpersGroup = new THREE.Group();
+
+helpersGroup.add(gridHelper, axesHelper);
+scene.add(helpersGroup);
+////////////////////////////////////////////////////////////
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const deltaTime = clock.getElapsedTime();
+
+  // // Update Camera
+  // const cameraSpeed = Math.PI;
+  // camera.position.set(
+  //   Math.sin(cursor.mouseX * Math.PI * 2) * cameraSpeed,
+  //   cursor.mouseY * Math.PI * 2,
+  //   Math.cos(cursor.mouseX * Math.PI * 2) * cameraSpeed
+  // );
+  // camera.lookAt(boxMesh.position);
+
+  // Controls have to be updated when using damping
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
