dc9b95f92f4cf0d44a6dbefe247737c9335e30aa
RaulCastillo <lraul.castillo@gmail.com>
2023-07-30 21:50:30 -0600

    anim and material

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { Quaternion } from "three";
 
 // Canvas
 const canvas = document.querySelector('.webgl');
@@ -17,21 +18,36 @@ const sizes =
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-    75,
+    50,
     sizes.width / sizes.height,
     0.01,
     1000
 );
-camera.position.z = 3
+camera.position.set(7,3,7);
+camera.lookAt(0,0,0);
 scene.add(camera);
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Box Object
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshNormalMaterial();
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+const boxRotation = new THREE.Quaternion()
+scene.add(boxMesh);
+
+
+
+/*
+Axis Helpers
+*/
+const gridSize = 10;
+const gridHelper = new THREE.GridHelper(gridSize, gridSize, 0x00eeff, 0xffffff);
+const axesHelper = new THREE.AxesHelper(3);
+
+const mainHelpersGroup = new THREE.Group();
+mainHelpersGroup.add(gridHelper, axesHelper);
+scene.add(mainHelpersGroup);
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer(
@@ -41,3 +57,25 @@ const renderer = new THREE.WebGLRenderer(
 );
 renderer.setSize(sizes.width,sizes.height);
 renderer.render(scene,camera);
+
+
+// Delta time (start time)
+let time = Date.now();
+// Animations
+
+const tick = () =>
+{
+
+    // Delta time
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+    // console.log(deltaTime);
+
+
+    boxMesh.rotation.y += 0.002 * deltaTime;
+    renderer.render(scene,camera);
+    window.requestAnimationFrame(tick);
+}
+
+tick();
